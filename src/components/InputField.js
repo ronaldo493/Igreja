4caf106f22fbc83e70026b0d1a91a1a15ebd3164
IgorViniCraft <jogogame97@gmail.com>
2024-02-09 16:00:51 -0300

    .
    .

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleInput, Input } from '../styles/map'; 
 
-const InputField = ({ type, placeholder, value, onChange }) => {
+const InputField = ({label, type, placeholder, value, onChange }) => {
     return (
         <StyleInput>
             <Input type={type} placeholder={placeholder} value={value} onChange={onChange} ></Input>
